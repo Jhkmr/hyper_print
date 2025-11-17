fe9aa7ad6f73a02f914d02027d89a0beace9754d
Jhkmr <jules.hoogkamer@gmail.com>
2025-11-17 11:39:39 +0100

    Flipped webcam & resolution

--- subsites/webcam/script.js
+++ subsites/webcam/script.js
@@ -3,11 +3,22 @@ const video = document.getElementById('webcam');
 async function startWebcam() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: false // optional: set true if you want mic too
+      video: {
+        width: { ideal: 4096 },
+        height: { ideal: 2160 }
+      }
     });
 
     video.srcObject = stream;
+
+    const track = stream.getVideoTracks()[0];
+    const cap = track.getCapabilities();
+
+    if ('zoom' in cap) {
+      await track.applyConstraints({
+        advanced: [{ zoom: 1 }]
+      });
+    }
 
   } catch (err) {
     console.error("Error accessing webcam:", err);
